f9e8a45a21177330b4e19667754c879a4829b09b
Diego Aguirre <diego3884@gmail.com>
2015-02-19 08:57:38 -0500

    Used a modified Single Application to create a base Empty Application file

---
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BlocBrowser
+//
+//  Created by Diego Aguirre on 2/19/15.
+//  Copyright (c) 2015 Diego Aguirre. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
